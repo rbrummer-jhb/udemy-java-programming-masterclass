54f724948ce0b7699acc98fc063a005364ab6722
rbrummer-jhb <rbrummer@student.wethinkcode.co.za>
2021-02-17 13:24:01 +0200

    added ReadingUserInput project

--- ReadingUserInput/src/com/njrbjava/Main.java
+++ ReadingUserInput/src/com/njrbjava/Main.java
@@ -9,15 +9,26 @@ public class Main {
 
         // scanner will parse the integer for us
         System.out.println("Enter your year of birth: ");
-        int yearOfBirth = scanner.nextInt();
-        scanner.nextLine(); // handle next line character (enter key)
 
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
-        int age = 2021 - yearOfBirth;
+        boolean hasNextInt = scanner.hasNextInt();
 
-        System.out.println("Your name is " + name + ", and you are " + age +
-                " years old.");
+        if (hasNextInt) {
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine(); // handle next line character (enter key)
+
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+            int age = 2021 - yearOfBirth;
+
+            if (age >= 0 && age <= 100) {
+                System.out.println("Your name is " + name +
+                        ", and you are " + age + " years old.");
+            } else {
+                System.out.println("Invalid year of birth.");
+            }
+        } else {
+            System.out.println("Unable to parse year of birth.");
+        }
 
         // close the scanner after using it so it can
         // release the underlying memory it was using
