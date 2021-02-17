cdbd3ba915597aa3f7b68a2c3667eaeda0206476
rbrummer-jhb <rbrummer@student.wethinkcode.co.za>
2021-02-17 09:55:29 +0200

    added ConstructorsPartTwo project

--- ConstructorsPartTwo/src/com/njrbjava/Main.java
+++ ConstructorsPartTwo/src/com/njrbjava/Main.java
@@ -8,9 +8,19 @@ public class Main {
 
         VipCustomer person2 = new VipCustomer("Bob", 25000.00);
         System.out.println(person2.getName());
+        System.out.println(person2.getEmailAddress());
 
         VipCustomer person3 = new VipCustomer("Tim", 100.00, "tim@email.com");
         System.out.println(person3.getName());
         System.out.println(person3.getEmailAddress());
+
+        VipCustomer person4 = new VipCustomer("Bruce",1500.0, "thebat@wayne.com");
+        System.out.println(person4.getName());
+        System.out.println(person4.getEmailAddress());
+
+        VipCustomer person5 = new VipCustomer("Tony", 1300.0);
+        System.out.println(person5.getName());
+        System.out.println(person5.getCreditLimit());
+        System.out.println(person5.getEmailAddress());
     }
 }
