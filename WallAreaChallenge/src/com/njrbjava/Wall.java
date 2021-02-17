db7dad645b43886aa54332b3a2448f8862086c49
rbrummer-jhb <rbrummer@student.wethinkcode.co.za>
2021-02-17 12:25:04 +0200

    added WallArea challenge

--- WallAreaChallenge/src/com/njrbjava/Wall.java
+++ WallAreaChallenge/src/com/njrbjava/Wall.java
@@ -5,40 +5,29 @@ public class Wall {
     private double width;
     private double height;
 
-    public Wall() {
-        this(10,20);
+    public Wall(double width, double height) {
+        this.width = width < 0 ? 0 : width;
+        this.height = height < 0 ? 0 : height;
     }
 
-    public Wall(double width, double height) {
-        if (width < 0 || height < 0) {
-            this.width = 0;
-            this.height = 0;
-        } else {
-            this.width = width;
-            this.height = height;
-        }
+    public Wall() {
+        this(0, 0);
     }
 
     public double getWidth() {
         return width;
     }
+
     public void setWidth(double width) {
-        if (width < 0) {
-            this.width = 0;
-        } else {
-            this.width = width;
-        }
+        this.width = width < 0 ? 0 : width;
     }
 
     public double getHeight() {
         return height;
     }
+
     public void setHeight(double height) {
-        if (height < 0) {
-            this.height = 0;
-        } else {
-            this.height = height;
-        }
+        this.height = height < 0 ? 0 : height;
     }
 
     public double getArea() {
