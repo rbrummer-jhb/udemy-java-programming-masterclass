a200970299a75ce504189afbdb93b8d910b066d7
rbrummer-jhb <rbrummer@student.wethinkcode.co.za>
2021-02-24 20:37:03 +0200

    added FirstAndLastDigitSum challenge

---
+++ FirstAndLastDigitSumChallenge/src/com/njrbjava/FirstLastDigitSum.java
@@ -0,0 +1,18 @@
+package com.njrbjava;
+
+public class FirstLastDigitSum {
+
+    public static int sumFirstAndLastDigit(int number) {
+        if (number < 0) {
+            return -1;
+        }
+
+        int lastDigit = number % 10;
+
+        while (number > 9) {
+            number /= 10;
+        }
+
+        return number + lastDigit;
+    }
+}
