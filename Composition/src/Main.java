601bfbcd1722e25d2d3a25d1815aa905f5ca1fc6
rbrummer-jhb <rbrummer@student.wethinkcode.co.za>
2021-03-26 10:58:40 +0200

    added Composition

--- Composition/src/Main.java
+++ Composition/src/Main.java
@@ -10,9 +10,6 @@ public class Main {
                 "v2.44");
 
         PC thePC = new PC(theCase, theMonitor, theMotherboard);
-
-        thePC.getMonitor().drawPixelAt(1500, 1200, "red");
-        thePC.getMotherboard().loadProgram("Windows 1.0");
-        thePC.getTheCase().pressPowerButton();
+        thePC.powerUp();
     }
 }
